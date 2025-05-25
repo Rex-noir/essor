e100e77341e644e6f27cd94801178e747da696d5
AceReX <ace.rexnoir@gmail.com>
2025-05-25 21:33:45 +0530

    refactor : improve refresh tokens handling
    - store refresh tokens inside database

--- auth/routes.go
+++ auth/routes.go
@@ -3,11 +3,13 @@ package auth
 import (
 	"fmt"
 	"habit_tracker/api/database"
+	utils "habit_tracker/api/internal/tokens"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -67,7 +69,7 @@ func RegisterRoutes(r *gin.RouterGroup, q *database.Queries) {
 			return
 		}
 
-		accessToken, err := GenerateJWT(createdUser.ID.String(), time.Minute*15)
+		accessToken, err := utils.GenerateJWT(createdUser.ID.String(), time.Minute*15)
 		if err != nil {
 			// Log the error for debugging purposes (e.g., with fmt.Errorf or a logger)
 			fmt.Printf("Error generating access token for user %s: %v\n", createdUser.ID.String(), err)
@@ -75,11 +77,29 @@ func RegisterRoutes(r *gin.RouterGroup, q *database.Queries) {
 			return
 		}
 
-		refreshToken, err := GenerateJWT(createdUser.ID.String(), time.Hour*24*7)
+		refreshToken := utils.GenerateSecureToken(64)
 		if err != nil {
 			// Log the error
 			fmt.Printf("Error generating refresh token for user %s: %v\n", createdUser.ID.String(), err)
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate refresh token"})
+			return
+		}
+
+		userAgent := ctx.GetHeader("User-Agent")
+		ipAddress := ctx.ClientIP()
+		expiresAt := time.Now().Add(7 * 24 * time.Hour)
+
+		params := database.CreateRefreshTokenParams{
+			UserID:    createdUser.ID,
+			Token:     refreshToken,
+			UserAgent: pgtype.Text{String: userAgent, Valid: userAgent != ""}, // assuming nullable columns, use pointers
+			IpAddress: pgtype.Text{String: ipAddress, Valid: ipAddress != ""},
+			ExpiresAt: pgtype.Timestamptz{Time: expiresAt, Valid: true},
+		}
+		// Store refresh token inside database
+		createdRefreshToken, err := q.CreateRefreshToken(ctx.Request.Context(), params)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create refresh token"})
 			return
 		}
 
@@ -90,7 +110,7 @@ func RegisterRoutes(r *gin.RouterGroup, q *database.Queries) {
 				Email:    createdUser.Email,
 			},
 			Token:        accessToken,
-			RefreshToken: refreshToken,
+			RefreshToken: createdRefreshToken.Token,
 		})
 
 	})
