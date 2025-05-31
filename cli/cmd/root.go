d8ac0bcd2b93bb3c82e98f93c3276d7c2ebef27f
AceReX <ace.rexnoir@gmail.com>
2025-05-31 13:13:12 +0530

    feat: update root command description for essor CLI
    This commit updates the root command description for the essor CLI to provide a more accurate and concise overview of its purpose.
    
    The following changes were made:
    
    - Updated the `Use` field to "essor".
    - Updated the `Short` field to "Essor cli for project use only.".
    - Updated the `Long` field to "This cli is an helper for essor project backend development.".
    
    These changes improve the clarity and usability of the CLI by providing a more relevant description of its functionality.

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -8,14 +8,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "essor",
+	Short: "Essor cli for project use only.",
+	Long:  `This cli is an helper for essor project backend development.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
