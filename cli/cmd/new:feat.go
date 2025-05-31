9c46fd6ccda580e4beefb9a8041c93fabc29398b
AceReX <ace.rexnoir@gmail.com>
2025-05-31 22:19:08 +0530

    feat: add new:feat command to generate feature files
    This commit introduces a new command `new:feat` that allows developers to quickly generate feature files within specified modules (api or database).
    
    The command takes the feature name as an argument and creates a directory with the feature name inside the target module. It also generates a basic Go file with the feature name as the package name.
    
    The target module can be specified using the `--api` or `--database` flags. If no flag is provided, the default target is the `api` module.

---
+++ cli/cmd/new:feat.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	apiFlag      bool
+	databaseFlag bool
+)
+
+// new:featCmd represents the new:feat command
+var newFeatCmd = &cobra.Command{
+	Use:   "new:feat",
+	Short: "Add new feature to the project.",
+	Long:  `This will write up new feature inside the specified application. Default is api.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		featureName := args[0]
+		target := "api"
+
+		if databaseFlag {
+			target = "database"
+		}
+
+		dirPath := filepath.Join(target, featureName)
+		err := os.MkdirAll(dirPath, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Failed to create directory: %v", err)
+		}
+
+		fileName := strings.ToLower(featureName) + ".go"
+		filePath := filepath.Join(dirPath, fileName)
+
+		content := fmt.Sprintf("package %s\n", featureName)
+
+		err = os.WriteFile(filePath, []byte(content), 0644)
+		if err != nil {
+			log.Fatalf("Failed to create file: %v", err)
+		}
+
+		fmt.Printf("Created feature '%s' in %s\n", featureName, dirPath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(newFeatCmd)
+
+	newFeatCmd.Flags().BoolVar(&apiFlag, "api", false, "Create feature in api module")
+	newFeatCmd.Flags().BoolVar(&databaseFlag, "database", false, "Create feature in database module")
+
+}
