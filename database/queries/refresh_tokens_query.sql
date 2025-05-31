fd03d226542daa781a9ce39de5c84097f0314c53
AceReX <ace.rexnoir@gmail.com>
2025-05-31 13:21:02 +0530

    refactor: move SQL queries to database/queries directory and update sqlc config
    This commit refactors the SQL queries by moving them to a dedicated `database/queries` directory. It also updates the `sqlc.yaml` configuration file to reflect this change.
    
    The following changes were made:
    
    - Moved `query.sql` to `database/queries/refresh_tokens_query.sql` and renamed it to be more specific.
    - Created a new file `database/queries/user_query.sql` to hold user-related queries.
    - Updated `sqlc.yaml` to point to the new `database/queries` directory for SQL queries.
    - Regenerated the Go code for the queries using sqlc.
    - Moved user queries from `database/refresh_tokens_query.sql.go` to `database/user_query.sql.go`.

--- database/queries/refresh_tokens_query.sql
+++ database/queries/refresh_tokens_query.sql
@@ -1,24 +1,3 @@
--- name: GetUserById :one
-SELECT * FROM users
-WHERE  id = $1 LIMIT 1;
-
--- name: ListUsers :many
-SELECT * FROM users
-ORDER BY created_at;
-
--- name: CreateUser :one
-INSERT INTO users (
-    username, email, password
-) VALUES (
-    $1, $2, $3
-)
-RETURNING *;
-
--- name: GetUserByEmail :one
-SELECT id, email, password, created_at, username
-FROM users
-WHERE email = $1;
-
 -- name: CreateRefreshToken :one
 INSERT INTO refresh_tokens (
     user_id, token, user_agent, ip_address, expires_at
