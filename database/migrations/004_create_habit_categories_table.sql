dcf78b9acf30b6ec4c3f51bf5214a1ba392ad201
AceReX <ace.rexnoir@gmail.com>
2025-06-01 13:06:38 +0530

    feat: add unique constraint to habit_categories table
    This commit adds a unique constraint to the `habit_categories` table, ensuring that each user can only have one habit category with the same name.
    
    The following changes were made:
    
    - Added `UNIQUE(user_id, name)` constraint to the `habit_categories` table.

--- database/migrations/004_create_habit_categories_table.sql
+++ database/migrations/004_create_habit_categories_table.sql
@@ -7,6 +7,8 @@ CREATE TABLE habit_categories (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     deleted_at TIMESTAMP WITH TIME ZONE NULL,
     sync_version BIGINT DEFAULT 1
+
+    UNIQUE(user_id, name)
 )
 ---- create above / drop below ----
 DROP TABLE IF EXISTS habit_categories;
