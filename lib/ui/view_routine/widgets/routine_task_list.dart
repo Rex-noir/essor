27f7368c194c65931b54605cd9ac6c2e4245fec5
AceReX <ace.rexnoir@gmail.com>
2025-06-29 16:00:05 +0530

    refactor : remove padding from the routine_task_list

--- lib/ui/view_routine/widgets/routine_task_list.dart
+++ lib/ui/view_routine/widgets/routine_task_list.dart
@@ -16,7 +16,6 @@ class RoutineTaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       itemCount: tasks.length,
       separatorBuilder: (context, index) => const SizedBox(height: 8),
       itemBuilder: (context, index) {
