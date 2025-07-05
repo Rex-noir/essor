a28fee225bd1a47e7b56c4925003d11a2b02053d
AceReX <ace.rexnoir@gmail.com>
2025-07-05 13:55:53 +0530

    feat (dependencies) : install flutter local notification and timezone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -28,6 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
